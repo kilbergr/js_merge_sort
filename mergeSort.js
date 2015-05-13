4e9d463b534841c5a3af67ba2484b8fbc5b685c4
kilbergr <rekilberg@gmail.com>
2015-05-13 14:42:14 -0700

    mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,14 +1,90 @@
+exports.merge = function(left, right){
+	var leftI = 0;
+		var rightI = 0;
+		var res = [];
+		while (leftI<left.length || rightI<right.length) {
+			if (leftI === left.length){
+				res.push(right[rightI]);
+				rightI++;
+			}
+			else if (rightI === right.length || left[leftI] <= right[rightI]){
+				res.push(left[leftI]);
+				leftI++;
+			}
+			else if (left[leftI] > right[rightI]) {
+				res.push(right[rightI]);
+				rightI++;
+			}
+		}
+		return res;
+	}
 
+exports.mergeSort = function(arr) {
+		if (arr.length < 2){
+			return arr;
+		}
+		var mid = Math.floor(arr.length/2);
+		var left = arr.slice(0, mid);
+		var right = arr.slice(mid, arr.length);
+		var sortedL = exports.mergeSort(left);
+		var sortedR = exports.mergeSort(right);
+		return exports.merge(sortedL, sortedR);
+	}
+
+
+/*First version of merge:
+
+module.exports = {
+	merge: function(left, right) {
+		var leftI = 0;
+		var rightI = 0;
+		var res = [];
+		while (leftI<left.length || rightI<right.length) {
+			if(left[leftI] <= right[rightI]){
+				res.push(left[leftI]);
+				leftI++;
+			}
+			else if(left[leftI] > right[rightI]) {
+				res.push(right[rightI]);
+				rightI++;
+			}
+			else if (leftI === left.length){
+				res.push(right[rightI]);
+				rightI++;
+			}
+			else if (rightI === right.length){
+				res.push(left[leftI]);
+				leftI++;
+			}
+		}
+		return res;
+	}
+}
+*/
+
+//Could refactor as:
+/*module.exports = {
+	merge: function(left, right) {
+		var leftI = 0;
+		var rightI = 0;
+		var res = [];
+		while (leftI<left.length || rightI<right.length) {
+			if (leftI === left.length){
+				res.push(right[rightI]);
+				rightI++;
+			}
+			else if (rightI === right.length || left[leftI] <= right[rightI]){
+				res.push(left[leftI]);
+				leftI++;
+			}
+			else if(left[leftI] > right[rightI]) {
+				res.push(right[rightI]);
+				rightI++;
+			}
+		}
+		return res;
+	}
+*/
 // Merge takes two sorted arrays (left, right) and
 // returns one array which contains the sorted numbers
-// from left and right.
-exports.merge = function(left, right) {
-}
-
-// Merge sort uses the merge function in order to
-// sort an array. Given an array of numbers in any
-// order, merge sort should return an array that is
-// sorted.
-exports.mergeSort = function(arr) {
-}
 
--- spec/mergeSortSpec.js
+++ spec/mergeSortSpec.js
@@ -5,11 +5,35 @@ describe('Merge', function() {
      expect(mergeSort.merge([1],[2])).toEqual([1,2]);
   });
 
+  it('should merge two arrays of size 2', function(){
+  	expect(mergeSort.merge([2, 3], [1, 5])).toEqual([1, 2, 3, 5]);
+  });
+
+  it('should merge two arrays of size 3 and 4', function(){
+  	expect(mergeSort.merge([2, 3, 6], [1, 4, 5, 9])).toEqual([1, 2, 3, 4, 5, 6, 9]);
+  });
+
+  it('should merge when left array is larger than right', function(){
+  	expect(mergeSort.merge([1, 4, 5, 9], [2, 3, 6])).toEqual([1, 2, 3, 4, 5, 6, 9]);
+  });
+
+  it('should merge when left array is all smaller than right', function() {
+  	expect(mergeSort.merge([1, 2, 3, 4], [5, 6, 9])).toEqual([1, 2, 3, 4, 5, 6, 9]);
+  });
+
 });
 
 describe('Merge Sort', function() {
-  it('should sort two arrays of size 1', function() {
-     expect(mergeSort.merge([2],[1])).toEqual([1,2]);
+  it('should sort an array of length 2', function() {
+     expect(mergeSort.mergeSort([2,1])).toEqual([1,2]);
+  });
+
+  it('should sort an array of length 4', function(){
+  	expect(mergeSort.mergeSort([2, 4, 1, 3])).toEqual([1, 2, 3, 4]);
+  });
+
+  it('should sort an array of length 7', function() {
+  	expect(mergeSort.mergeSort([2, 4, 5, 7, 3, 1, 8])).toEqual([1, 2, 3, 4, 5, 7, 8]);
   });
 });
 
